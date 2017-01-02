6bc101ead62182800e8f158bff0cedd826876cb9
Lou17 <Lou17@LOURDES>
2017-01-02 11:39:41 -0500

    Delete references to postsharp

--- Angkor.O7Framework.Domain/O7DomainException.cs
+++ Angkor.O7Framework.Domain/O7DomainException.cs
@@ -1,29 +1,29 @@
-﻿// Create by Felix A. Bueno
+﻿//// Create by Felix A. Bueno
 
-using System;
-using System.Reflection;
-using Angkor.O7Framework.Domain.Response;
-using Oracle.ManagedDataAccess.Client;
-using PostSharp.Aspects;
-using PostSharp.Serialization;
+//using System;
+//using System.Reflection;
+//using Angkor.O7Framework.Domain.Response;
+//using Oracle.ManagedDataAccess.Client;
+//using PostSharp.Aspects;
+//using PostSharp.Serialization;
 
-namespace Angkor.O7Framework.Domain
-{
-    [PSerializable]
-    public class O7DomainExceptionAttribute : OnExceptionAspect
-    {
-        public override void OnException(MethodExecutionArgs args)
-        {                                    
-            args.FlowBehavior = FlowBehavior.Return;
-            args.ReturnValue = build_response(args.Exception);
-        }
+//namespace Angkor.O7Framework.Domain
+//{
+//    [PSerializable]
+//    public class O7DomainExceptionAttribute : OnExceptionAspect
+//    {
+//        public override void OnException(MethodExecutionArgs args)
+//        {                                    
+//            args.FlowBehavior = FlowBehavior.Return;
+//            args.ReturnValue = build_response(args.Exception);
+//        }
 
-        private static O7ErrorResponse build_response(Exception exception)
-        {
-            var oracleException = exception as OracleException;
-            return oracleException == null
-                ? new O7ErrorResponse(400, $"Message:{exception.Message}")
-                : new O7ErrorResponse(401, $"Code:{oracleException.ErrorCode} Message:{oracleException.Message}");
-        }
-    }
-}
+//        private static O7ErrorResponse build_response(Exception exception)
+//        {
+//            var oracleException = exception as OracleException;
+//            return oracleException == null
+//                ? new O7ErrorResponse(400, $"Message:{exception.Message}")
+//                : new O7ErrorResponse(401, $"Code:{oracleException.ErrorCode} Message:{oracleException.Message}");
+//        }
+//    }
+//}
