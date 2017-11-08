1ab1d42af4aec7c0d09fe7c477b4538944be85d1
FBUENO <FBUENO@DESKTOP-4D6F4QS>
2017-11-07 19:18:22 -0500

    Add ExcludeFilter

---
+++ Angkor.O7Framework.Web/Attributes/ExcludeFilterAttribute.cs
@@ -0,0 +1,13 @@
+﻿// Solution: O7Framework
+// Owner: FBUENO
+// Date: 07 - 11 - 2017
+
+using System;
+
+namespace Angkor.O7Framework.Web.Attributes
+{
+    public class ExcludeFilterAttribute : Attribute
+    {
+        
+    }
+}
