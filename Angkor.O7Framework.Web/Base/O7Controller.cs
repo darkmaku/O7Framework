b0467afebbc4b2276d4e29e22e439318c414a80a
gianfranco <a20121776@pucp.pe>
2017-12-07 16:47:46 -0500

    add error controller

--- Angkor.O7Framework.Web/Base/O7Controller.cs
+++ Angkor.O7Framework.Web/Base/O7Controller.cs
@@ -19,6 +19,12 @@ namespace Angkor.O7Framework.Web.Base
                 return new O7Principal(user.Login, user.Company, user.Branch, user.Name, user.Password,user.UserApplication,user.Attributes);                
             }
         }
+
+        public ActionResult ReturnToServerError(string message)
+        {
+            return Redirect(LinkHelper.SourceLink("Error", "ServerError") + "?message=" + HttpUtility.UrlEncode(message));
+        }
+
         public ActionResult ReturnToAuthorizationError(string message)
         {
             return Redirect(LinkHelper.SourceLink("Error", "AuthorizationError") + "?message=" + HttpUtility.UrlEncode(message));
