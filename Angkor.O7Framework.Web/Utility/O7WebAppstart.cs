04d8b49b33b65398b6d96581fce85b83cdd7d94f
FBUENO <FBUENO@DESKTOP-4D6F4QS>
2018-02-23 15:16:14 -0500

    changes

---
+++ Angkor.O7Framework.Web/Utility/O7WebAppstart.cs
@@ -0,0 +1,32 @@
+﻿// Solution: O7Framework
+// Owner: FBUENO
+// Date: 23 - 02 - 2018
+
+using System.Configuration;
+
+namespace Angkor.O7Framework.Web.Utility
+{
+    public class O7WebAppstart : ConfigurationSection
+    {
+        [ConfigurationProperty("namespace")]
+        public string Namespace
+        {
+            get => (string)base["namespace"];
+            set => base["namespace"] = value;
+        }
+
+        [ConfigurationProperty("action")]
+        public string Action
+        {
+            get => (string)base["action"];
+            set => base["action"] = value;
+        }
+
+        [ConfigurationProperty("controller")]
+        public string Controller
+        {
+            get => (string)base["controller"];
+            set => base["controller"] = value;
+        }
+    }
+}
